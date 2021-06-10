4165e5934452718a96bef58aa262dbb1b65ef18a
Anthony Mugendi <ngurumugz@gmail.com>
2021-06-10 18:43:49 +0300

    open android files via termux

--- openurl.js
+++ openurl.js
@@ -1,19 +1,23 @@
-var spawn = require('child_process').spawn;
+var spawn = require("child_process").spawn;
 
 var command;
 
-switch(process.platform) {
-    case 'darwin':
-        command = 'open';
-        break;
-    case 'win32':
-        command = 'explorer.exe';
-        break;
-    case 'linux':
-        command = 'xdg-open';
-        break;
-    default:
-        throw new Error('Unsupported platform: ' + process.platform);
+switch (process.platform) {
+  case "darwin":
+    command = "open";
+    break;
+  case "win32":
+    command = "explorer.exe";
+    break;
+  case "linux":
+    command = "xdg-open";
+    break;
+  // try open android using termux
+  case "android":
+    command = "termux-open";
+    break;
+  default:
+    throw new Error("Unsupported platform: " + process.platform);
 }
 
 /**
@@ -24,24 +28,24 @@ switch(process.platform) {
  */
 
 function open(url, callback) {
-    var child = spawn(command, [url]);
-    var errorText = "";
-    child.stderr.setEncoding('utf8');
-    child.stderr.on('data', function (data) {
-        errorText += data;
-    });
-    child.stderr.on('end', function () {
-        if (errorText.length > 0) {
-            var error = new Error(errorText);
-            if (callback) {
-                callback(error);
-            } else {
-                throw error;
-            }
-        } else if (callback) {
-            callback(error);
-        }
-    });
+  var child = spawn(command, [url]);
+  var errorText = "";
+  child.stderr.setEncoding("utf8");
+  child.stderr.on("data", function (data) {
+    errorText += data;
+  });
+  child.stderr.on("end", function () {
+    if (errorText.length > 0) {
+      var error = new Error(errorText);
+      if (callback) {
+        callback(error);
+      } else {
+        throw error;
+      }
+    } else if (callback) {
+      callback(error);
+    }
+  });
 }
 
 /**
@@ -50,18 +54,18 @@ function open(url, callback) {
  * @param recipientsSeparator Default is ",". Use ";" for Outlook.
  */
 function mailto(recipients, fields, recipientsSeparator, callback) {
-    recipientsSeparator = recipientsSeparator || ",";
+  recipientsSeparator = recipientsSeparator || ",";
 
-    var url = "mailto:"+recipients.join(recipientsSeparator);
-    Object.keys(fields).forEach(function (key, index) {
-        if (index === 0) {
-            url += "?";
-        } else {
-            url += "&";
-        }
-        url += key + "=" + encodeURIComponent(fields[key]);
-    });
-    open(url, callback);
+  var url = "mailto:" + recipients.join(recipientsSeparator);
+  Object.keys(fields).forEach(function (key, index) {
+    if (index === 0) {
+      url += "?";
+    } else {
+      url += "&";
+    }
+    url += key + "=" + encodeURIComponent(fields[key]);
+  });
+  open(url, callback);
 }
 
 exports.open = open;
